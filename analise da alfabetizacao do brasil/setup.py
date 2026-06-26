6e4d3af4c6ba52188950de2bc3c2764d4b68c45a
diego nascimento <diego.m1nascimento@gmail.com>
2026-06-26 19:52:29 -0300

    primeiro commit

---
+++ analise da alfabetizacao do brasil/setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='Tech Challenge 2 - Análise da Alfabetização no Brasil',
+    author='Diego & Matheus',
+    license='MIT',
+)
